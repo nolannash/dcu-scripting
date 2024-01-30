8803f0a763b84f4ed9e9517008e01ce3ba3c6319
Nolan <n0lan.n4sh@gmail.com>
2024-01-29 17:16:46 -0800

    test running scan then apply updates, needs work

--- 4_apply_updates/4.1_dcu_manual_apply_all_updates.ps1
+++ 4_apply_updates/4.1_dcu_manual_apply_all_updates.ps1
@@ -6,20 +6,22 @@ if (Test-Path $dcuCliPath -PathType Leaf) {
 
     foreach ($type in $updateTypes) {
         # Run a scan for a specific update type
-        $scanResult = Start-Process -FilePath $dcuCliPath -ArgumentList "/scan -updateType=$type -autoSuspendBitLocker=enable" -Wait -PassThru
+        $scanResult = Start-Process -FilePath $dcuCliPath -ArgumentList "/scan -updateType=$type" -Wait -PassThru -NoNewWindow 
 
         if ($scanResult.ExitCode -eq 1) {
             Write-Host "Updates found for $type. Applying updates..." -ForegroundColor Green
 
             # Apply updates for a specific type without reboot
-            Start-Process -FilePath $dcuCliPath -ArgumentList "/applyUpdates -updateType=$type -reboot=disable -outputlog=C:\Users\$env:username\Desktop\dcuUpdateLog_$type.log" -Wait -NoNewWindow -WindowStyle Hidden
+            Start-Process -FilePath $dcuCliPath -ArgumentList "/applyUpdates -updateType=$type -reboot=disable -autoSuspendBitLocker=enable" -Wait -NoNewWindow -WindowStyle Hidden
 
             Write-Host "$type updates applied successfully." -ForegroundColor Green
         } else {
-            Write-Host "No updates found for $type." -ForegroundColor Green
+            Write-Host "No updates found for $type." -ForegroundColor Yellow
         }
     }
 } else {
     Write-Host "Error: Dell Command Update CLI (dcu-cli.exe) not found at $dcuCliPath." -ForegroundColor Red
 }
+
+# Pause to keep the PowerShell window open for manual inspection
 Pause
