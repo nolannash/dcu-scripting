21eb7c18c2854a5468115ecea4bb23a3f56124f0
nolan nash <n0lan.n4sh@gmail.com>
2024-08-23 16:02:32 -0700

    take 3 gitignore

--- DCU_extra/dcu_scan.ps1
+++
@@ -1,3 +0,0 @@
-Start-Process -Path "C:\Program Files\Dell\CommandUpdate\dcu-cli.exe" -ArgumentList "/scan -updateType=bios,firmware,driver -autoSuspendBitLocker=enable" -outputlog=""C:\Users\$env:username\Desktop\dcuUpdateLog.log"""
-
-Pause
